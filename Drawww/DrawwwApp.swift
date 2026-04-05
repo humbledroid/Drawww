15740f597862ca13d80bb6cfb47129a1117ab170
humbledroid <carotkut12@gmail.com>
2026-04-05 23:25:56 +0530

    Initial Commit

---
+++ Drawww/DrawwwApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawwwApp.swift
+//  Drawww
+//
+//  Created by Deathcode on 05/04/26.
+//
+
+import SwiftUI
+
+@main
+struct DrawwwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
